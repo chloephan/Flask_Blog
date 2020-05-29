71b06a321f1daf97fc6c6e5267180a0901a33fb2
chloephan <anhpn97@gmail.com>
2020-05-28 23:09:01 -0700

    has bug, reset email/password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask 
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd752d08fd9322d12acb5e432a4fafc63'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -10,5 +13,11 @@ bcrypt = Bcrypt(app)
 login_manager= LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'infor'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
